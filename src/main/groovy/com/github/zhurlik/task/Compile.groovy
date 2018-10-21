f34644ae4fa0f0685a47ccf08143c67f4f2fccdc
zhurlik <zhurlik@gmail.com>
2018-10-21 15:47:07 +0300

    one more task

---
+++ src/main/groovy/com/github/zhurlik/task/Compile.groovy
@@ -0,0 +1,36 @@
+package com.github.zhurlik.task
+
+import com.github.zhurlik.extension.ArduinoIde
+import com.github.zhurlik.option.Actions
+import com.github.zhurlik.option.ArduinoBuilderOptions
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class Compile extends DefaultTask {
+    ArduinoBuilderOptions options = new ArduinoBuilderOptions([action: Actions.COMPILE])
+    // sometimes there is a need to modify options manually
+    Closure optinsCustomizer = {}
+    String sketch
+
+    @TaskAction
+    void action() {
+        final String ideHome = project.extensions.getByType(ArduinoIde).homeDir
+
+        options.addHardware(ideHome)
+        options.addTools(ideHome)
+        options.addBuiltInLibraries(ideHome)
+        options.addLibraries()
+        options.addPrefs(ideHome)
+        options.customize(optinsCustomizer)
+        options.validate(Actions.COMPILE)
+
+        final List<String> args = new LinkedList<>()
+        args.add("${ideHome}/arduino-builder")
+        args.addAll(options.convertToArgs())
+        args.add(sketch)
+
+        project.exec {
+            commandLine args
+        }
+    }
+}
